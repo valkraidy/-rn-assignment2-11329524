b9883d24ec48603020db69e8c64019fec80f45c9
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2024-05-25 00:39:20 -1200

    project completd

---
+++ Assignment/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My name is  <Text style={styles.textBold}>Kwame Val kraidy Solomon</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontSize:24
+  },
+  textBold:{
+    fontWeight:'bold'
+  }
+});
